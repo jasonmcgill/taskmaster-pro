42f91dd949aab32197c98f43b271a6906ec37afa
jasonmcgill <64054045+jasonmcgill@users.noreply.github.com>
2020-05-28 14:58:40 -0600

    added moment dates and audit dates to change urgency and show it

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var createTask = function (taskText, taskDate, taskList) {
 	// append span and p element to parent li
 	taskLi.append(taskSpan, taskP);
 
+	//check due date
+	auditTask(taskLi);
 
 	// append to ul list on the page
 	$("#list-" + taskList).append(taskLi);
@@ -104,12 +106,17 @@ $(".list-group").on("click", "span", function () {
 	//swap out elements
 	$(this).replaceWith(dateInput);
 
+	// enable jquery ui datepicker
+	dateInput.datepicker({
+	minDate: 1
+	});
+
 	//automatically focus on new element
 	dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function () {
+$(".list-group").on("change", "input[type='text']", function () {
 	// get current text
 	var date = $(this)
 		.val()
@@ -137,6 +144,9 @@ $(".list-group").on("blur", "input[type='text']", function () {
 
 	//replace input with span element
 	$(this).replaceWith(taskSpan);
+
+	// Pass task's <li> element into auditTask() to check new due date
+	auditTask($(taskSpan).closest(".list-group-item"));
 })
 
 
@@ -241,6 +251,39 @@ $('#trash').droppable({
 	}
 
 });
+
+//add moment.js date picker to modal
+$("#modalDueDate").datepicker( {
+	minDate: 1,
+	onClose: function() {
+		//when calendar is closed, force a ""change" even on the 'dateInput'
+		$(this).trigger("change");
+	}
+});
+
+var auditTask = function(taskEl) {
+	// get date from task element
+	var date = $(taskEl).find("span").text().trim();
+
+	// convert to moment object at 5:00pm
+	var time = moment(date, "L").set("hour", 17);
+  
+	// remove any old classes from element
+	$(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+  
+	// apply new class if task is near/over due date
+	if (moment().isAfter(time)) {
+	  $(taskEl).addClass("list-group-item-danger");
+	}
+
+	// apply new class if task is near/over due date
+	if (moment().isAfter(time)) {
+		$(taskEl).addClass("list-group-item-danger");
+	  } 
+	  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+		$(taskEl).addClass("list-group-item-warning");
+	  }
+}
 
 // load tasks for the first time
 loadTasks();
